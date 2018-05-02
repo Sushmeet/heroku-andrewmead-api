b784d8a92ce3c0eeaaf35c4797165cd653f71733
Sushmeet Sunger <ssunger@shutterstock.com>
2018-05-02 15:47:50 -0400

    hashing on playground

---
+++ playground/hashing1.js
@@ -0,0 +1,38 @@
+const crypto = require("crypto");
+const { SHA256 } = require("crypto");
+
+const message = "I am user number 3";
+
+const hashify = message => {
+  return crypto
+    .createHash("sha256")
+    .update(message)
+    .digest("hex");
+};
+
+const data = {
+  id: 4
+};
+
+const token = {
+  data,
+  hash: hashify(`${data} + somesecret`)
+};
+
+
+// Man in the middle attack
+// someone changes id and sends new hash
+
+// token.data.id = 5;
+// token.hash = hashify(JSON.stringify(token.data));
+
+const resultHash = hashify(`${token.data} + somesecret`);
+
+console.log(token.hash);
+console.log(resultHash);
+
+if (resultHash === token.hash) {
+  console.log("Trust the hash");
+} else {
+  console.log("Do not trust the hash");
+}
---
+++ playground/hashing2.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+const data = {
+    id: 10
+}
+
+
+
+const token = jwt.sign(data, 'secret')
+
+const decoded = jwt.verify(token, 'secret');
+
+// jwt.verify()
+
+
+console.log('Token', token);
+console.log('Verified and Decoded token', decoded);
