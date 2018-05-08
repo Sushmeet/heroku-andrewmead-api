7770784605fa0359e4ca32492c92b06d377c9666
Sushmeet Sunger <ssunger@shutterstock.com>
2018-05-08 11:31:36 -0400

    clean u

--- server/server.js
+++ server/server.js
@@ -12,6 +12,48 @@ const port = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the TODO API");
+});
+
+app.get("/todos", (req, res) => {
+  Todo.find().then(todos => {
+    res.status(200).send({ todos });
+  });
+});
+
+app.get("/todos/:id", (req, res) => {
+  const id = req.params.id;
+
+  if (!ObjectId.isValid(id)) {
+    return res.status(404).send();
+  } else {
+    Todo.findById(id)
+      .then(todo => {
+        if (!todo) {
+          return res.status(404).send({});
+        }
+        res.send({ todo });
+      })
+      .catch(e => {
+        res.status(400).send();
+      });
+  }
+});
+
+app.post("/todos", (req, res) => {
+  const todo = new Todo({ text: req.body.text });
+
+  todo.save().then(
+    todos => {
+      res.status(200).send({ todos });
+    },
+    e => {
+      res.status(400).send(e);
+    }
+  );
+});
+
 app.patch("/todos/:id", (req, res) => {
   const id = req.params.id;
   const body = _.pick(req.body, ["text", "completed"]);
@@ -63,58 +105,6 @@ app.delete("/todos/:id", (req, res) => {
     .catch(e => {
       return res.status(400).send(e);
     });
-});
-
-app.post("/todos", (req, res) => {
-  const todo = new Todo({ text: req.body.text });
-
-  todo.save().then(
-    todos => {
-      res.status(200).send({ todos });
-    },
-    e => {
-      res.status(400).send(e);
-    }
-  );
-});
-
-app.get("/todos", (req, res) => {
-  // callback style
-  // Todo.find((err, todos) => {
-  //   if (err) return console.error(err);
-  //   console.log('Get request', todos);
-  //   res.status(200).send(todos);
-  // })
-  // console.log("URL", req.url);
-  // console.log("Body", req.body);
-  // console.log("Route", req.route);
-
-  Todo.find().then(todos => {
-    res.status(200).send({ todos });
-  });
-});
-
-app.get("/todos/:id", (req, res) => {
-  const id = req.params.id;
-
-  if (!ObjectId.isValid(id)) {
-    return res.status(404).send();
-  } else {
-    Todo.findById(id)
-      .then(todo => {
-        if (!todo) {
-          return res.status(404).send({});
-        }
-        res.send({ todo });
-      })
-      .catch(e => {
-        res.status(400).send();
-      });
-  }
-});
-
-app.get("/", (req, res) => {
-  res.send("Welcome to the TODO API");
 });
 
 // POST username
