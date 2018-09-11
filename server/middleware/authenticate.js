1aa68aa75e7542b89d8358fda9bfe70ef1e89657
Sushmeet Sunger <ssunger@shutterstock.com>
2018-09-11 15:26:17 -0400

    all tests working

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -1,7 +1,7 @@
-const { User } = require("../models/user");
+const { User } = require('../models/user');
 
 const authenticate = (req, res, next) => {
-  const token = req.header("x-auth");
+  const token = req.header('x-auth');
 
   User.findByToken(token)
     .then(user => {
@@ -13,7 +13,7 @@ const authenticate = (req, res, next) => {
       next();
     })
     .catch(e => {
-      res.status(401).send("No user");
+      res.status(401).send(`No user${e}`);
     });
 };
 
--- server/test/seed-data/seed.js
+++ server/test/seed-data/seed.js
@@ -1,6 +1,8 @@
 const { ObjectID } = require('mongodb');
+const jwt = require('jsonwebtoken');
 
 const { Todo } = require('../../models/todo');
+const { User } = require('../../models/user');
 
 const todoVals = [
   {
@@ -13,10 +15,33 @@ const todoVals = [
   }
 ];
 
+const userOneID = new ObjectID();
+const userTwoID = new ObjectID();
+
+
+const users = [
+  {
+    _id: userOneID,
+    email: 'sushi@gmail.com',
+    password: 'blah',
+    token: [
+      {
+        access: 'auth',
+        jwt: jwt.sign({ _id: userOneID, access: 'auth' }, 'secretValue').toString()
+      }
+    ]
+  },
+  {
+    _id: userTwoID,
+    email: 'sushi23232@gmail.com',
+    password: 'blah'
+  }
+];
 // const insertTodos = () => Todo.insertMany(todoVals).then((res) => {});
 const insertTodos = async () => Todo.insertMany(todoVals);
 
 const deleteTodos = async () => Todo.remove({});
+
 
 module.exports = {
   todoVals,
